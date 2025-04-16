9fbc3a0e45be21d50ff6bf0b8a611d7b69eacdf6
wrferreira1003 <wrf.wellington@gmail.com>
2025-04-16 10:19:21 -0300

    feat(layout): integrates header

--- modules/feature/layout/src/index.ts
+++ modules/feature/layout/src/index.ts
@@ -1,5 +1,3 @@
 export * from './lib/layout.module';
 
-export * from './lib/header.component';
-
 export * from './lib/header/header.component';
--- modules/feature/layout/src/lib/header/header.component.ts
+++ modules/feature/layout/src/lib/header/header.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
   selector: 'lib-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  standalone: true,
 })
 export class HeaderComponent {}

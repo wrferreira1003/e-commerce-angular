53a292961ddc9fab7fe3ccfa5586f28e144e8e2f
wrferreira1003 <wrf.wellington@gmail.com>
2025-04-17 10:41:12 -0300

    feat(layout): add header

--- modules/feature/layout/src/lib/layout.module.ts
+++ modules/feature/layout/src/lib/layout.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 
+/* Aqui se declara os componentes relacionados so layout */
+
 @NgModule({
-  imports: [CommonModule, HeaderComponent],
-  exports: [HeaderComponent],
+  imports: [CommonModule, HeaderComponent], //Entrada do modulo
+  exports: [HeaderComponent], //Saida final do modulo
 })
 export class LayoutModule {}

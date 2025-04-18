a136f586701bcc443dbb8d470e70dbb70d6ee57d
wrferreira1003 <wrf.wellington@gmail.com>
2025-04-17 22:24:15 -0300

    feat(product-data-acess): add product

---
+++ modules/data-acess/product/src/lib/service/product-search/product-search.service.spec.ts
@@ -0,0 +1,42 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { MockProducts } from '../../mocks/products.mocks';
+import { Product } from '../../models/product.model';
+import { ProductSearchService } from './product-search.service';
+
+describe('ProductSearchService', () => {
+  let service: ProductSearchService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(ProductSearchService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return products correctly', () => {
+    // Arrange - Preparação
+    const mockName = 'notebook';
+    const url = `${service.API_URL}/products?name=${mockName}`;
+    let result: Product[] = [];
+
+    // Act - Ação
+    service.searchByName(mockName).subscribe((products) => (result = products));
+
+    // Assert - Verificação
+    const request = httpMock.expectOne(url);
+    request.flush(MockProducts);
+    expect(request.request.method).toBe('GET');
+    expect(result).toEqual(MockProducts);
+    expect(request.request.method).not.toBe('POST');
+  });
+});

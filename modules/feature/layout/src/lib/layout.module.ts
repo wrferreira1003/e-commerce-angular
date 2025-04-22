429b328a712b1fbe6b953952de01181824d70182
wrferreira1003 <wrf.wellington@gmail.com>
2025-04-22 11:15:26 -0300

    refactor(layout): remove unnecessary comment in layout module

--- modules/feature/layout/src/lib/layout.module.ts
+++ modules/feature/layout/src/lib/layout.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 
-/* Aqui se declara os componentes relacionados so layout */
-
 @NgModule({
   imports: [CommonModule, HeaderComponent], //Entrada do modulo
   exports: [HeaderComponent], //Saida final do modulo

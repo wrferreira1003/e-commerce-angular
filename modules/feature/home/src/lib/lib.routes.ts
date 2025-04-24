69e5b696a3c468d2c052a300a149ac8e1e44f104
wrferreira1003 <wrf.wellington@gmail.com>
2025-04-24 09:27:38 -0300

    feat(routes): implement lazy loading for home route and set default redirection

--- modules/feature/home/src/lib/lib.routes.ts
+++ modules/feature/home/src/lib/lib.routes.ts
@@ -1,4 +1,19 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-export const homeRoutes: Route[] = [{ path: '', component: HomeComponent }];
+export const homeRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+
+    // Exemplo apenas que podemos ter um lazy loading para a rota filha do home.
+    // children: [
+    // {
+    //     path: 'home', //Rota home
+    //     //Lazy loading com o loadComponent
+    //     loadChildren: () =>
+    //     import('@ecommerce/product').then((m) => m.productRoutes),
+    // },
+    // ],
+  },
+];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '', //Rota padrão
+    pathMatch: 'full', //Redireciona para a rota home
+    redirectTo: 'home', //Redireciona para a rota home
+  },
+  {
+    path: 'home', //Rota home
+    //Lazy loading com o loadComponent
+    loadChildren: () => import('@ecommerce/home').then((m) => m.homeRoutes),
+  },
+];

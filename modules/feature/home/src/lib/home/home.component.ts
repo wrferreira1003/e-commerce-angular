e7101ec36b11f9863d054e242e6a2c6cf6a33821
wrferreira1003 <wrf.wellington@gmail.com>
2025-04-25 09:59:59 -0300

    feat(home): implement product display in home component with styling

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MockProducts } from '@ecommerce/product-data-acess';
 
 @Component({
   selector: 'lib-home',
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  products = MockProducts;
+}

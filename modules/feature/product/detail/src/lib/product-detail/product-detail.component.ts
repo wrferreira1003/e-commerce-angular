e1c03263ea0d0beab8712518a8bbe86d0535e538
wrferreira1003 <wrf.wellington@gmail.com>
2025-04-30 18:26:41 -0300

    feat(tsconfig): add path mapping for product detail module

---
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-product-detail',
+  imports: [CommonModule],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.scss',
+})
+export class ProductDetailComponent {}

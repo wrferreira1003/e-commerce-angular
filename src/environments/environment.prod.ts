e060b456261819663daa4f61238bb730b0af5e2c
wrferreira1003 <wrf.wellington@gmail.com>
2025-05-12 09:59:27 -0300

    chore: adiciona arquivos de ambiente

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  // Adicione aqui outras variáveis de ambiente de produção
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  // Adicione aqui outras variáveis de ambiente de desenvolvimento
+};
